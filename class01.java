6e46db366dcd334c82b59d006a01ce4d69ff0187
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-10-11 17:52:19 +0800

    Create class01.java

---
+++ class01.java
@@ -0,0 +1,18 @@
+class CRectangle{
+    int width;
+    int height;
+   
+void CRectangle(int w,int h){
+        width=w;
+        height=h;
+        /*w=10;
+        h=8;*/
+        this.width=10;
+        this.height=8;
+        }
+}
+public class classo1{
+	public static void main(String[] args) {
+		
+	}
+}
