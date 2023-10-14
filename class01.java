fc55d9adc8c839b99d72b76aa96dba83343b4b6c
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-10-14 21:34:35 +0800

    Update class01.java

--- class01.java
+++ class01.java
@@ -1,18 +1,28 @@
 class CRectangle{
-    int width;
-    int height;
-   
-void CRectangle(int w,int h){
-        width=w;
-        height=h;
-        /*w=10;
-        h=8;*/
-        this.width=10;
-        this.height=8;
-        }
+  int width;
+  int height;
+  public CRectangle(int w,int h){
+    width=w;
+    height=h;
+    System.out.println("constructor CRectangle(int w,int h) called"); 
+    }
+
+    public CRectangle(){
+        width=10;
+        height=8;
+        System.out.println("constructor CRectangle() called");
+    }
+    public void show(){
+        System.out.println("width="+width);
+        System.out.println("height="+height);
+        
+    }
+    
 }
-public class classo1{
+public class class01
+{
 	public static void main(String[] args) {
-		
+		CRectangle rec1=new CRectangle(5,2);
+	
 	}
 }
